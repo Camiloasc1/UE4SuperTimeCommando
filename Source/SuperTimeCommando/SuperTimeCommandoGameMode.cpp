8c5bf362cd0f033adea7f5d9fbba39d3ed316dad
Camilo Sanchez <caasanchezcu@unal.edu.co>
2016-12-27 12:29:20 -0500

    UE4 Top Down Template

---
+++ Source/SuperTimeCommando/SuperTimeCommandoGameMode.cpp
@@ -0,0 +1,19 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "SuperTimeCommando.h"
+#include "SuperTimeCommandoGameMode.h"
+#include "SuperTimeCommandoPlayerController.h"
+#include "SuperTimeCommandoCharacter.h"
+
+ASuperTimeCommandoGameMode::ASuperTimeCommandoGameMode()
+{
+	// use our custom PlayerController class
+	PlayerControllerClass = ASuperTimeCommandoPlayerController::StaticClass();
+
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDownCPP/Blueprints/TopDownCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
