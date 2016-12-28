836d59bf6811626ec68bda5b774844ce63ec61da
Camilo Sanchez <caasanchezcu@unal.edu.co>
2016-12-27 19:02:21 -0500

    Corners in the LoSObstacle

--- Source/SuperTimeCommando/LoSObstacle.cpp
+++ Source/SuperTimeCommando/LoSObstacle.cpp
@@ -7,16 +7,21 @@
 // Sets default values
 ALoSObstacle::ALoSObstacle()
 {
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	RootComponent = Mesh;
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
 void ALoSObstacle::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	if (Corners.Num() == 0)
+	{
+		UpdateCorners();
+	}
 }
 
 // Called every frame
@@ -26,3 +31,19 @@ void ALoSObstacle::Tick( float DeltaTime )
 
 }
 
+// Calculates the corners based on the actor's bounds
+void ALoSObstacle::UpdateCorners()
+{
+	FVector Orgin;
+	FVector BoxExtent;
+	GetActorBounds(false, Orgin, BoxExtent);
+	/*FString CoordinateString = FString::Printf(TEXT("Character Position is %s"), *Orgin.ToCompactString());
+	UE_LOG(LogActor, Warning, TEXT("%s"), *CoordinateString);
+	CoordinateString = FString::Printf(TEXT("Bounds Extent is %s"), *BoxExtent.ToCompactString());
+	UE_LOG(LogActor, Warning, TEXT("%s"), *CoordinateString);*/
+	Corners.Empty();
+	Corners.Add(FVector2D(Orgin.X + BoxExtent.X, Orgin.Y + BoxExtent.Y));
+	Corners.Add(FVector2D(Orgin.X + BoxExtent.X, Orgin.Y - BoxExtent.Y));
+	Corners.Add(FVector2D(Orgin.X - BoxExtent.X, Orgin.Y + BoxExtent.Y));
+	Corners.Add(FVector2D(Orgin.X - BoxExtent.X, Orgin.Y - BoxExtent.Y));
+}
--- Source/SuperTimeCommando/LoSObstacle.h
+++ Source/SuperTimeCommando/LoSObstacle.h
@@ -9,17 +9,29 @@ UCLASS()
 class SUPERTIMECOMMANDO_API ALoSObstacle : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+		
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	class UStaticMeshComponent* Mesh;
+
+protected:
+	// The corners of the actor
+	// If empty the corners will be automatically calculated based on the actor's bounds
+	UPROPERTY(EditAnywhere, Category = "Obstacle")
+	TArray<FVector2D> Corners;
+
+public:
 	// Sets default values for this actor's properties
 	ALoSObstacle();
 
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	
-	// Called every frame
-	virtual void Tick( float DeltaSeconds ) override;
 
-	
-	
+	// Called every frame
+	virtual void Tick(float DeltaSeconds) override;
+
+public:
+	// Calculates the corners based on the actor's bounds
+	virtual void UpdateCorners();
+
 };
