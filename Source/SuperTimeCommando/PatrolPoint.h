57a3d9dd06d7798362cf7e501eba098a1ac757e6
Camilo Sanchez <caasanchezcu@unal.edu.co>
2016-12-30 22:18:13 -0500

    PatrolPoint improved
    With a decal component in the editor

--- Source/SuperTimeCommando/PatrolPoint.h
+++ Source/SuperTimeCommando/PatrolPoint.h
@@ -13,13 +13,4 @@ class SUPERTIMECOMMANDO_API APatrolPoint : public AActor
 public:	
 	// Sets default values for this actor's properties
 	APatrolPoint();
-
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-	
-	// Called every frame
-	virtual void Tick( float DeltaSeconds ) override;
-
-	
-	
 };
